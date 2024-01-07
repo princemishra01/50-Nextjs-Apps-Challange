38004fbf66ddd548a9d4d0ba3d20ebe1941ec975
Prince <prince.pm.mishra@gmail.com>
2024-01-08 00:02:45 +0530

    completed 02 quote generator

---
+++ 02-random-quote-generator/src/app/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='flex flex-col justify-center items-center'>
+        <div className='mt-[100px]'>
+            Created By - 
+            <a href="https://www.linkedin.com/in/manishk4514/" target="_blank" className='ml-1'> Manish Kumar</a> &
+            <a href="https://www.linkedin.com/in/princemishra01/" target="_blank" className='ml-1'> Prince Mishra</a>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
