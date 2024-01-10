fe8f4b630f50498c053534e9149c3f3a2c722855
Prince <prince.pm.mishra@gmail.com>
2024-01-10 23:31:02 +0530

    04 tic tac toe completed

--- 04-tic-tac-toe/src/app/page.jsx
+++ 04-tic-tac-toe/src/app/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 import Image from 'next/image'
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 // 1 2 3
 // 4 5 6
@@ -22,8 +22,8 @@ export default function Home() {
   const [ board, setBoard ] = useState(initialBoard);
   const [user, setUser] = useState('O');
   const [message, setMessage] = useState('');
-  
-  // console.log();
+
+// console.log();
 
   function togglePlayer() {
     setUser((user) => user === 'O' ? 'X' : 'O');
@@ -49,51 +49,59 @@ export default function Home() {
       let [a, b, c] = winingCombos[i];
       console.log(board[a.toString()], board[b.toString()], board[c.toString()]);
       if(board[a.toString()] !== " " && board[a.toString()] === board[b.toString()] && board[a.toString()] === board[c.toString()]) {
-        return user === 'O' ? "Player 1 wins" : "Player 2 wins";
+        return user === 'O' ? "Player 1 wins 🥳" : "Player 2 wins 🥳";
       }
     }
     
     return null;
   }
 
-  async function markPlayer (e) {
-    if(board[e.target.id] !== " ") return;
+  function markPlayer (e) {
+    if(board[e.target.id] !== " " || message) return;
 
-    await setBoard((board) => {
+    setBoard((board) => {
       return {
         ...board,
         [e.target.id]: user
       }
     });
+  }
 
+  const resetGame = () => {
+    setBoard(initialBoard);
+    setMessage('');
+    setUser('O');
+  }
+
+  useEffect(() => { 
     const player = isWon();
     console.log(player);
-
     if(player){
       setMessage(player)
     }
-    
     togglePlayer();
-  }
+  }, [board] );
   
   return (
-    <main>
-      <h1>
+    <main className='flex flex-col items-center text-8xl'>
+      <h1 className='mt-8'>
         Tic Tac Toe
       </h1>
-      <div className='grid grid-rows-3 grid-cols-3 text-white'>
+      <div className='inline-grid grid-rows-3 grid-cols-3 gap-1 text-white mt-[30px]'>
         {
           Object.keys(board).map((boardVal, index) =>{
             return (
               // TODO:
-              <div className='bg-slate-400' onClick={markPlayer} key={index} id={index + 1}>{board[boardVal] + "+"}</div>
+              <div className='bg-slate-400 w-[100px] h-[100px] flex items-center justify-center' onClick={markPlayer} key={index} id={index + 1}>{board[boardVal]}</div>
             )
           })
         }
       </div>
-      <h1>
+      <h1 className='text-4xl pt-8'>
         {message}
       </h1>
+
+      <button className='text-black bg-white p-3 rounded-2xl text-lg mt-5' onClick={resetGame}>Restart</button>
 
     </main>
   )
